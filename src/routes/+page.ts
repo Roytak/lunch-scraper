def4321686cda3fe4bb7413dce15ba00c27cee36
Roytak <xjanot04@fit.vutbr.cz>
2024-09-08 22:20:07 +0200

    add base

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,7 +1,8 @@
 import type { PageLoad } from './$types'
+import { base } from '$app/paths'
 
 export const load: PageLoad  = async ({ fetch }) => {
-    const res = await fetch('/api/lunch-scraper');
+    const res = await fetch(`${base}/api/lunch-scraper`);
     if (!res.ok) {
         throw new Error('Failed to fetch lunch menu');
     }
