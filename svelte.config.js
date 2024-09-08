0ae0de481a29d3334119737dcec1c37b2ceb10a5
Roytak <xjanot04@fit.vutbr.cz>
2024-09-08 21:57:32 +0200

    deploy 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
 	kit: {
 		adapter: adapter({
-			fallback: '200.html' // may differ from host to host
-		})
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
+
+export default config;
