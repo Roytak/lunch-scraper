041f93236d10f35cb8ebc7a829e49cda771a81ab
roman <xjanot04@fit.vutbr.cz>
2024-09-09 13:36:07 +0200

    vercel deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,12 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
+
 	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+		adapter: adapter()
 	}
 };
 
