8c257a7e52234d33c27de5eb7af878b69bec309e
Roytak <xjanot04@fit.vutbr.cz>
2024-09-11 19:57:04 +0200

    disable prerender x2

--- src/routes/api/lunch-scraper/+server.ts
+++ src/routes/api/lunch-scraper/+server.ts
@@ -1,6 +1,8 @@
 import type { RequestHandler } from './$types'
 import { LunchScraper } from '$lib/server/lunchScraper'
 
+export const prerender = false;
+
 export const GET: RequestHandler = async () => {
 	const scraper = LunchScraper.instance;
 	const lunches = await scraper.getLunchMenus();
