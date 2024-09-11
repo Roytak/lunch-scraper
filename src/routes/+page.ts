fb1d5da94783ed92043ce0439e3057c15d142265
Roytak <xjanot04@fit.vutbr.cz>
2024-09-11 19:37:30 +0200

    remove base

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,8 +1,7 @@
 import type { PageLoad } from './$types'
-import { base } from '$app/paths'
 
 export const load: PageLoad  = async ({ fetch }) => {
-    const res = await fetch(`${base}/api/lunch-scraper`);
+    const res = await fetch(`/api/lunch-scraper`);
     if (res.ok) {
         const lunches = await res.json();
         return { lunches };   
